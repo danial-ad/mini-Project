709e41b24dd853057d8ffb57f2672e4034de2e38
danial <ahmaddaniel668@gmail.com>
2024-01-04 11:02:32 +0800

    function login for admin and user

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -20,6 +20,7 @@
                 <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
             </div>
 
+
             <div class="mt-4">
                 <x-label for="password" value="{{ __('Password') }}" />
                 <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
